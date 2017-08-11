e7f2a3c14fdbeb8ebd6fc99e880aea9ecd5856ac
増田貢士 <pgpg110@gmail.com>
2017-08-11 11:20:20 +0900

    Lesson7 2Dゲームアプリを作ろう 課題提出コミット
    課題：キューブの接触時に効果音をつけましょう

---
+++ Assets/CubeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	// キューブの移動速度
+	private float speed = -0.2f;
+
+	// 消滅位置
+	private float deadLine = -10;
+
+	// 地面の位置
+	private float groundLevel = -3.0f;
+
+	//ブロック落下音
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start(){
+		//ブロック落下音
+		audioSource = gameObject.GetComponent<AudioSource>();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// キューブを移動させる
+		transform.Translate (this.speed, 0, 0);
+
+		// 画面外に出たら破棄する
+		if (transform.position.x < this.deadLine){
+			Destroy (gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		//障害物に衝突した場合音を出す。
+		if (other.gameObject.tag == "CubeTag" || other.gameObject.tag == "GroundTag") {
+			audioSource.Play ();
+		}
+
+	}
+}
